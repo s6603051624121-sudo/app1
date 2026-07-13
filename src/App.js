d8319ec6370070f29b069b310e5ce6395bbe41cb
s6603051624121-sudo <s6603051624121@email.kmutnb.ac.th>
2026-07-13 11:16:27 +0700

    Update App.js with css external

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-import './App.css';
+import './style.css';
 
 function App() {
-  const divStyle = {
-    color: 'red',
-    backgroundColor: 'powderblue',
-    fontSize: 'larger',
-    padding: '3px',
-
-  }
   return (
-    <div style={divStyle}>Hello React</div>
+    <>
+    <h2>DeveloperThai.com</h2>
+    <div className='title'>React & React Native</div>
+    </>
   );
 }
 
