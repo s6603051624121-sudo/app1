c6248d01e4aca7f52a928d7fd0910b2b48e4eddf
s6603051624121-sudo <s6603051624121@email.kmutnb.ac.th>
2026-07-13 10:55:32 +0700

    Update App.js with insert variable

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let r = 'React'
+    let rn = 'React Native'
+    const el = (
+      <div>
+        Create Web & Mobile Apps<br/>
+        with {r} and {rn}
+      </div>
+    )
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    el
   );
 }
 
