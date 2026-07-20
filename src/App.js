e360349c05704bdfbe7486479bc3d1df2d89ad13
s6603051624121-sudo <s6603051624121@email.kmutnb.ac.th>
2026-07-20 10:32:01 +0700

    Create components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import './style.css';
+import React from 'react';
+import { Header, Content, Footer } from './func-components';
 
 function App() {
   return (
     <>
-    <h2>DeveloperThai.com</h2>
-    <div className='title'>React & React Native</div>
+      <Header/>
+      <Content/>
+      <Footer/>
     </>
   );
 }
