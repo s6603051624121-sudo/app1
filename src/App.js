6ed04cbd955c69e0c1ffa5a37213f222f784b4d4
s6603051624121-sudo <s6603051624121@email.kmutnb.ac.th>
2026-08-03 10:08:32 +0700

    add calculator.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import React from 'react';
-import { Header, Content, Footer } from './func-components';
-import Calender from './class-components';
+import { Calculator2 } from "./calculator";
 
 function App() {
   return (
-    <>
-      <Header/>
-      <p><center><Calender/></center></p>
-      <Content/>
-      <Footer/>
-    </>
-  );
+    <Calculator2 />
+  )
 }
 
 export default App;
