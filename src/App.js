87a1e1e42348bb5398825c859042f9a099d1eda8
s6603051624121-sudo <s6603051624121@email.kmutnb.ac.th>
2026-08-10 09:56:28 +0700

    add refs-func.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { EventData2 } from "./event-data";
+import RefsFunc from "./refs-func";
 
 function App() {
   return (
-    <EventData2/>
+    <RefsFunc/>
   )
 }
 
