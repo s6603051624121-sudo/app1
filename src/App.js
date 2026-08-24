6841256708880e6361bb0dd1b32287898577b262
s6603051624121-sudo <s6603051624121@email.kmutnb.ac.th>
2026-08-24 10:41:44 +0700

    add context

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import Effect from './effect';
+import React from 'react'
+import { userContext } from './context'
+import Header2 from './context-header2'
+import Content2 from './context-content2'
 
-function App() {
-  return (
-    <Effect/>
-  )
+export default function App() {
+    let [user, setUser] = React.useState('')
+    return (
+        <userContext.Provider value={[user, setUser]}>
+            <Header2/>
+            <Content2/>
+        </userContext.Provider>
+    )
 }
-
-export default App;
---
+++ src/context.js
@@ -0,0 +1,4 @@
+import React, { createContext } from 'react'
+
+export const userContext = createContext() //หรือ React.createContext()
+/* เนื่องจากเป็นตัวแปร ดังนั้น จึงส่งออกแบบ default ไม่ได้ */
